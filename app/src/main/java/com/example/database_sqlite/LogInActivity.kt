c22fa8c220712cf4acf5513c5714441d9bf2cb3a
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2021-07-12 18:50:49 -0300

    activity log in

--- app/src/main/java/com/example/database_sqlite/LogInActivity.kt
+++ app/src/main/java/com/example/database_sqlite/LogInActivity.kt
@@ -1,7 +1,25 @@
 package com.example.database_sqlite
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LogInActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
+        comenzar()
+    }
+    private fun comenzar(){
+
+        /**BOTON**/
+        val backButton = findViewById<Button>(R.id.back_button)
+        /**USER & PASSWORD**/
+        val userText = findViewById<EditText>(R.id.user_edit_text)
+        val passwordText = findViewById<EditText>(R.id.password_edit_text)
+        val repeatPasswordText = findViewById<EditText>(R.id.repeat_password_edit_text)
+
+    }
 }
