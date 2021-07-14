25487b9e5429351c69f175c4a8054d37f05f60d2
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2021-07-14 17:36:17 -0300

    modificaciones en miSQLiteHelper

--- app/src/main/java/com/example/database_sqlite/miSQLiteHelper.kt
+++ app/src/main/java/com/example/database_sqlite/miSQLiteHelper.kt
@@ -5,22 +5,21 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class miSQLiteHelper(context: Context):SQLiteOpenHelper(
-    context,"dataBase.db",null,1) {
+class miSQLiteHelper:SQLiteOpenHelper(
+    AppUsuarios.CONTEXT,AppUsuarios.DB_NAME,null,AppUsuarios.VERSION) {
+
+    val qryCreateTable = "CREATE TABLE ${AppUsuarios.DB_NAME}("+
+            "${Contract.Usuario.userID} STRING PRIMARY KEY,"+
+            "${Contract.Usuario.nombre} STRING,"+
+            "${Contract.Usuario.password} STRING"
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val ordenCreacion = "CREATE TABLE usuario "+
-                "(nombre STRING PRIMARY KEY," +
-                "contraseña STRING)"
-
-        db!!.execSQL(ordenCreacion)
+        db!!.execSQL(qryCreateTable)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        val ordenBorrado = "DROP TABLE IF EXISTS dabaBase"
 
-        db!!.execSQL(ordenBorrado)
-
+        db!!.execSQL("DROP TABLE IF EXISTS $qryCreateTable")
         onCreate(db)
     }
 
